4d6762c01aa631a62c1aaea34df284a836799159
VinzOo93 <v.orru@live.fr>
2022-12-22 13:24:12 +0100

    create develop => installing project on local envirement Ok First Json reponse OK

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ApiController extends AbstractController
+{
+    #[Route('/api', name: 'app_api')]
+    public function index(): Response
+    {
+        return new JsonResponse(
+            ['cash' => '500 euros']
+        );
+    }
+}
